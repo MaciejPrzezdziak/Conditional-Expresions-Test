20b24c4df591625182a7aa5a1982178321a528ea
Mateusz Osypiński <mateusz.osypinski@logicit.eu>
2016-11-26 11:29:50 +0100

    dodalem petle zeby program chodzil w kolko

--- sieiee.py
+++ sieiee.py
@@ -1,23 +1,30 @@
-Liczba1 = input("Podaj pierwszą liczbę")
+while true:
+	Liczba1 = input("Podaj pierwszą liczbę")
 
-if not Liczba1.isnumeric():
-    print("Podaj liczbę!!!")
-    quit()    
+	if not Liczba1.isnumeric():
+		print("Podaj liczbę!!!")
+		quit()    
 
-Liczba2 = input("Podaj drugą liczbę")
+	Liczba2 = input("Podaj drugą liczbę")
 
-if not Liczba2.isnumeric():
-    print("Podaj liczbe!!1")
-    quit()
+	if not Liczba2.isnumeric():
+		print("Podaj liczbe!!1")
+		quit()
 
-if Liczba1 > Liczba2 :
-    print("Pierwsza liczba jest większa od drugiej")
+	if Liczba1 > Liczba2 :
+		print("Pierwsza liczba jest większa od drugiej")
 
-elif Liczba2 > Liczba1 :
-    print("Druga liczba jest wieksza od pierwszej")
+	elif Liczba2 > Liczba1 :
+		print("Druga liczba jest wieksza od pierwszej")
 
-else:
-    print("Liczba jest równa")
+	else:
+		print("Liczba jest równa")
+		
+	odpowiedz = input("chcesz wyjsc? y/n")
+	
+	if odpowiedz == 'y' or odpowiedz == 'Y':
+		break
+	
 
 
 
